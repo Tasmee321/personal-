383d2c85d392fe9e594ca092ef3969cd2dc79fde
Tasmee Ul Hasnain <f24barin1e02125@iub.edu.pk>
2026-08-13 19:03:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ async function verifyEVM(txHash, expectedAddr, expectedAmt, network) {
       ];
 
       const TRANSFER_TOPIC =
-        "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a8df523b3ef";
+        "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
 
       let receipt = null;
       let latestBlock = null;
